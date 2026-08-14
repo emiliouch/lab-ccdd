c618ff73276bcf44fa367b7032cac319f915e9d8
Emilio Torres <emilio.torres@ug.uchile.cl>
2026-08-14 17:36:38 -0400

    feat: agregar etapa 3 y 4

--- labs/lab_2/src/agroalerta/sensores.py
+++ labs/lab_2/src/agroalerta/sensores.py
@@ -1,35 +1,38 @@
-class Sensor:
+from abc import ABC, abstractmethod
+
+
+class Sensor(ABC):
     def __init__(self, nombre: str, unidad: str) -> None:
         self.nombre = nombre
         self.unidad = unidad
 
-    def es_riesgo(self, valor: float) -> bool:
-        return False
+    @abstractmethod
+    def es_riesgo(self, valor: float) -> bool: ...
 
 
 class SensorTemperatura(Sensor):
     def __init__(self, minimo: float, maximo: float) -> None:
         super().__init__("temperatura", "°C")
-        self.minimo = minimo
-        self.maximo = maximo
+        self._minimo = minimo
+        self._maximo = maximo
 
     def es_riesgo(self, valor: float) -> bool:
-        return valor < self.minimo or valor > self.maximo
+        return valor < self._minimo or valor > self._maximo
 
 
 class SensorViento(Sensor):
     def __init__(self, maximo: float) -> None:
         super().__init__("viento", "km/h")
-        self.maximo = maximo
+        self._maximo = maximo
 
     def es_riesgo(self, valor: float) -> bool:
-        return valor > self.maximo
+        return valor > self._maximo
 
 
 class SensorHumedad(Sensor):
     def __init__(self, maximo: float) -> None:
         super().__init__("humedad", "%")
-        self.maximo = maximo
+        self._maximo = maximo
 
     def es_riesgo(self, valor: float) -> bool:
-        return valor > self.maximo
+        return valor > self._maximo
